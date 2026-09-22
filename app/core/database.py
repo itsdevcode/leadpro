0851007c694ba1ce9cedcb539e5d234fee5afe10
Arun Yadav <itsdevcode@gmail.com>
2026-09-22 00:34:15 +0000

    feat: add database configuration, alembic, and initial models

--- app/core/database.py
+++ app/core/database.py
@@ -1,1 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclearativeBase
+from app.core.config import settings
 
+
+engine = create_engine(settings.DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclearativeBase):
+    pass
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
