b61e85213beb7b5f41a8ebbf0ece566969a46d38
Arun Yadav <itsdevcode@gmail.com>
2026-09-22 16:49:06 +0000

    feat: add users and otps database models and migration

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,8 @@ from sqlalchemy import pool
 from alembic import context
 from app.core.database import Base
 from app.core.config import settings
+from app.models.user import User
+from app.models.otp import Otp
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
--- app/core/database.py
+++ app/core/database.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclearativeBase
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.core.config import settings
 
-
 engine = create_engine(settings.DATABASE_URL, echo=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-class Base(DeclearativeBase):
+class Base(DeclarativeBase):
     pass
 
 def get_db():
